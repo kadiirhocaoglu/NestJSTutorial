12470de6748c660613ebcef380e49521cd2ece7d
kadiirhocaoglu <kadiirhocaoglu@hotmail.com>
2024-09-24 14:22:20 +0300

    messages app without dependency injection

--- messages/src/messages/repositories/messages.repository.ts
+++ messages/src/messages/repositories/messages.repository.ts
@@ -11,6 +11,7 @@ export class MessagesRepository {
 
     async findAll() {
         const contents = await readFile('messages.json', 'utf8');
+        
         const messages = JSON.parse(contents);
         return messages;
     }
